65982fb8a031872d28a69731a8fa258a3585144a
Maksimandr <maksimandr@mail.ru>
2021-11-28 23:46:21 +0500

    lesson 1 ДЗ к первому уроку

--- lesson1/src/main/java/ru/gb/Product.java
+++ lesson1/src/main/java/ru/gb/Product.java
@@ -8,11 +8,14 @@ public class Product {
     private String title;
     private int cost;
 
+    /**
+     * Класс продукта
+     */
     public Product() {
-        this.id = count++;
+        this.id = count++; // автоинкремент для всех экземпляров класса
         this.title = "";
         for (int i = 0; i < 1 + id / 26; i++) {
-            this.title += String.valueOf((char) ((id % 26) + 65));
+            this.title += String.valueOf((char) ((id % 26) + 65)); // наименование состоит из символов А-Z, AA-ZZ, AAA-ZZZ ...
         }
         this.cost = (id + 1) * 10;
     }
--- lesson1/src/main/java/ru/gb/ProductsServlet.java
+++ lesson1/src/main/java/ru/gb/ProductsServlet.java
@@ -6,6 +6,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * Класс сервлет
+ */
 public class ProductsServlet extends HttpServlet {
 
     @Override
