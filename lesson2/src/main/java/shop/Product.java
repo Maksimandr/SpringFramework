8aec85b74cc9cb4f6248740901489225314f291d
Maksimandr <maksimandr@mail.ru>
2021-12-01 18:48:13 +0500

    lesson 2 ДЗ ко второму уроку

---
+++ lesson2/src/main/java/shop/Product.java
@@ -0,0 +1,50 @@
+package shop;
+
+/**
+ * Класс продукта
+ */
+public class Product {
+
+    private Integer id;
+    private String title;
+    private Integer cost;
+
+    public Product(Integer id, String title, Integer cost) {
+        this.id = id;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", cost=" + cost +
+                '}';
+    }
+}
